01053394da9a2076f2095ac023d4cbd35f0bdd21
Samarth Patil <60141449+imsamarthpatil@users.noreply.github.com>
2022-06-13 13:11:52 +0530

    Add files via upload

---
+++ Internet of Things (IoT)/Blood Bank Monitoring System/conf.py
@@ -0,0 +1,7 @@
+bolt_api_key = "XXXXXXXX"
+device_id = "BOLTXXXXXXXX"
+telegram_chat_id = "@XXXXXXXX"
+telegram_bot_id = "botXXXXXXXX"
+threshold = [20.48, 61.44, 102.4]
+frame_size = 10
+mul_factor = 2
